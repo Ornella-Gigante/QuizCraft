9303faddf5887bca871ebca26d9a650ad62e1ab2
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-05-10 00:39:59 +0200

    feat: Add ViewModel and updated Room dependencies using AndroidX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,17 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+    // Room Libraires
+    // Room (AndroidX)
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
+
+    // Kotlin annotation processing tool (kapt) si usas Kotlin:
+    // kapt("androidx.room:room-compiler:2.6.1")
+    // ViewModel y LiveData (AndroidX)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 }
