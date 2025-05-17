8d0d583da23a64f7d914ce59ca48d4505d02d0e8
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-05-18 00:46:05 +0200

    feat: addition of variables for questions, answer and database model by id

--- app/src/main/java/es/ifp/quizcraft/Questions.java
+++ app/src/main/java/es/ifp/quizcraft/Questions.java
@@ -1,4 +1,22 @@
 package es.ifp.quizcraft;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+
+@Entity(tableName = "questions_table")
 public class Questions {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+    private String question;
+    private String opA;
+    private String opB;
+    private String opC;
+    private int answer;
+
+
+
 }
