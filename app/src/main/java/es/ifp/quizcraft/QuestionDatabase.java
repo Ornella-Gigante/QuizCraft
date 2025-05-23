17870f0594194ca27afc7a5169c8b3cf64ac9075
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-05-23 15:08:56 +0200

    refactor: Replace deprecated AsyncTask with ExecutorService for database initialization

--- app/src/main/java/es/ifp/quizcraft/QuestionDatabase.java
+++ app/src/main/java/es/ifp/quizcraft/QuestionDatabase.java
@@ -1,16 +1,12 @@
 package es.ifp.quizcraft;
 
 import android.content.Context;
-import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
-import androidx.room.DatabaseConfiguration;
-import androidx.room.InvalidationTracker;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
-import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,50 +14,45 @@ import java.util.concurrent.Executors;
 @Database(entities = {Questions.class}, version = 1)
 public abstract class QuestionDatabase extends RoomDatabase {
 
-    private static QuestionDatabase INSTANCE;
-
-    public abstract QuestionDao questionDao();
-
+    private static volatile QuestionDatabase INSTANCE;
     private static final ExecutorService databaseWriteExecutor =
             Executors.newFixedThreadPool(1);
 
-   public static synchronized QuestionDatabase getInstance(final Context context){
+    public abstract QuestionDao questionDao();
 
-       if(INSTANCE == null){
-           INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-           QuestionDatabase.class, "questions_database")
-                   .fallbackToDestructiveMigration()
-                   .addCallback(RoomDBCallback)
-                   .build();
-       }
+    public static synchronized QuestionDatabase getInstance(final Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            QuestionDatabase.class,
+                            "questions_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(RoomDBCallback)
+                    .build();
+        }
+        return INSTANCE;
+    }
 
-       return INSTANCE;
-   }
+    // Callback usando ExecutorService
+    private static final RoomDatabase.Callback RoomDBCallback =
+            new RoomDatabase.Callback() {
+                @Override
+                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                    super.onCreate(db);
 
-
-   private static RoomDatabase.Callback RoomDBCallback = new RoomDatabase.Callback(){
-
-       @Override
-       public void onCreate(@NonNull SupportSQLiteDatabase db){
-           super.onCreate(db);
-       }
-   };
-
-
-   private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void>{
-
-       private QuestionDao questionDao;
-
-       private PopulateDbAsyncTask(QuestionDatabase db){
-
-           questionDao = db.questionDao();
-
-       }
-
-       @Override
-       protected  Void doInBackground(Void...voids){
-           return null;
-       }
-   }
-
+                    // Usar el Executor para operaciones en background
+                    databaseWriteExecutor.execute(() -> {
+                        QuestionDao dao = INSTANCE.questionDao();
+                        // Insertar datos iniciales
+                        dao.insert(new Questions(
+                                "What is Android?",
+                                "OS",
+                                "Browser",
+                                "Software",
+                                "Hard Drive",
+                                1
+                        ));
+                    });
+                }
+            };
 }
