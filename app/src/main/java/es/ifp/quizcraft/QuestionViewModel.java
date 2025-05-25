3e7a8b7f48d27bdfe452bff34a09932864efcb88
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-05-25 17:44:13 +0200

    refactor: Extend QuestionViewModel from AndroidViewModel for application context support

--- app/src/main/java/es/ifp/quizcraft/QuestionViewModel.java
+++ app/src/main/java/es/ifp/quizcraft/QuestionViewModel.java
@@ -2,16 +2,19 @@ package es.ifp.quizcraft;
 
 import android.app.Application;
 
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
-public class QuestionViewModel {
+public class QuestionViewModel extends AndroidViewModel {
 
     private QuestionsRepository mRepository;
     private LiveData<List<Questions>> mAllQuestions;
 
     public QuestionViewModel(Application application){
+
+        super(application);
 
         mRepository = new QuestionsRepository(application);
         mAllQuestions = mRepository.getAllQuestions();
