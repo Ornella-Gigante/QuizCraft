f12e57e190dd8795ba911055dc34fdbcc45cd507
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-06-01 23:19:26 +0200

    feat: add FinalScoreActivity to show score and allow quiz restart

--- app/src/main/java/es/ifp/quizcraft/FinalScoreActivity.java
+++ app/src/main/java/es/ifp/quizcraft/FinalScoreActivity.java
@@ -1,6 +1,9 @@
 package es.ifp.quizcraft;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,22 @@ public class FinalScoreActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_final_score);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.final_score_dialog);
+
+        // Recibe el score y total desde el Intent
+        int score = getIntent().getIntExtra("SCORE", 0);
+        int total = getIntent().getIntExtra("TOTAL", 0);
+
+        TextView txtFinalScore = findViewById(R.id.txtFinalScore);
+        txtFinalScore.setText("You got " + score + " out of " + total + " correct!");
+
+        Button btnRestart = findViewById(R.id.btnRestart);
+        btnRestart.setOnClickListener(v -> {
+            // Reinicia el quiz (puedes volver a la MainActivity o QuizActivity)
+            Intent intent = new Intent(FinalScoreActivity.this, QuizActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
+
 }
