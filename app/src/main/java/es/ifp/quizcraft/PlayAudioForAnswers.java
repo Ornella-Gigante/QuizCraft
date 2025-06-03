f6e913badb80b42922f34eb8563568ee18914a17
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-06-03 22:39:21 +0200

    feat: Create  variables for Context and MediaPlayer and Constructor

--- app/src/main/java/es/ifp/quizcraft/PlayAudioForAnswers.java
+++ app/src/main/java/es/ifp/quizcraft/PlayAudioForAnswers.java
@@ -1,4 +1,15 @@
 package es.ifp.quizcraft;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+
 public class PlayAudioForAnswers {
+
+    private Context mContext;
+    private MediaPlayer mediaPlayer;
+
+
+    public PlayAudioForAnswers(Context mContext) {
+        this.mContext = mContext;
+    }
 }
